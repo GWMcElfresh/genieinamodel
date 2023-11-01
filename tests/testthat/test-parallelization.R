6a405e7609fe411187937848aa44bf8b683d4d5f
GW McElfresh <mcelfreshgw@gmail.com>
2023-11-01 11:34:04 -0700

    Update test-parallelization.R

--- tests/testthat/test-parallelization.R
+++ tests/testthat/test-parallelization.R
@@ -8,15 +8,12 @@ test_that("Parallelization Works", {
   ##################################################
   
   library(tidyverse)
-  library(brms)
+  #create local cmdstanr directory and point to it for brms
   library(cmdstanr)
-  
-  ##################################################
-  # cmdStan install to local directory
-  ##################################################
   dir.create('/cmdstan', showWarnings = FALSE)
   cmdstanr::install_cmdstan(dir='/cmdstan')
   cmdstanr::set_cmdstan_path(path = list.dirs('/cmdstan')[[2]])
+  library(brms)
   
   ##################################################
   # Main
