9deef0810cc3f06358099d9ca428243eeea1589f
GWMcElfresh <mcelfreshgw@gmail.com>
2025-05-01 09:46:18 -0700

    fix dependencies, shamefully hide viz script until ready

--- R/Visualization.R
+++ R/Visualization.R
@@ -1,19 +1,19 @@
 library(copula)
 library(GGally)
-library(dplyr)
+
 
 # sample copula 
-Z_post <- posterior::as_draws_matrix(fit$draws("Z"))
+#Z_post <- posterior::as_draws_matrix(fit$draws("Z"))
 # Average over draws to get posterior mean of Z
-Z_hat <- matrix(colMeans(Z_post), nrow = N, ncol = P, byrow = FALSE)
+#Z_hat <- matrix(colMeans(Z_post), nrow = N, ncol = P, byrow = FALSE)
 
 # Compute pseudo-obs
-U <- pnorm( scale(Z_hat) )
+#U <- pnorm( scale(Z_hat) )
 
 #pairs_
-ggpairs(as.data.frame(U),
-        columns = 1:P,
-        upper = list(continuous = wrap("cor", size = 3)),
-        diag  = list(continuous = "densityDiag"),
-        lower = list(continuous = "points")) +
-  theme_minimal()
+#ggpairs(as.data.frame(U),
+#        columns = 1:P,
+#        upper = list(continuous = wrap("cor", size = 3)),
+#        diag  = list(continuous = "densityDiag"),
+#        lower = list(continuous = "points")) +
+#  theme_minimal()
